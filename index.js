b139265c0c952074924bb9f244392d1a1696bfca
조영준 <122534567+cho1124@users.noreply.github.com>
2025-10-29 20:18:42 +0900

    [Fix] index.js use 순서 변경

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ const PORT = 3000;
 
 app.use(express.json());
 app.use('/api/auction', auctionRoutes);
+app.use("/api/auth", authRoutes);
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('✅ Connected to MongoDB'))
   .catch(err => console.error('❌ MongoDB connection error:', err));
 
-app.use("/api/auth", authRoutes);
+
 
 app.listen(PORT, "0.0.0.0", () => console.log(`✅ Server running on http://localhost:${PORT}`));
