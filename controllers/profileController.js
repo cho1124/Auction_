4975f02ea6c8dfbb55602c786d83df56abacae7b
조영준 <122534567+cho1124@users.noreply.github.com>
2025-10-31 16:25:31 +0900

    [Fix] GetProfile 함수 이동

---
+++ controllers/profileController.js
@@ -0,0 +1,8 @@
+export const getProfile = async (req, res) => {
+  try {
+    const user = req.user;
+    res.status(200).json({ user });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,11 +1,3 @@
-export const getProfile = async (req, res) => {
-  try {
-    const user = req.user;
-    res.status(200).json({ user });
-  } catch (error) {
-    res.status(500).json({ message: "Server error" });
-  }
-};
 import express from "express";
 import { getProfile } from "../controllers/profileController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
