7dc17b27f8639396a700157c60436a19ed0a7b6c
조영준 <122534567+cho1124@users.noreply.github.com>
2025-10-31 16:18:42 +0900

    [Feat] 토큰 인증

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import auctionRoutes from './routes/auctionRoutes.js';
 import authRoutes from './routes/authRoutes.js';
+import profileRoutes from './routes/profileRoutes.js';
 import dotenv from 'dotenv';
 import "./scheduler.js";
 
@@ -12,6 +13,7 @@ const PORT = 3000;
 app.use(express.json());
 app.use('/api/auction', auctionRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/user", profileRoutes);
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('✅ Connected to MongoDB'))
